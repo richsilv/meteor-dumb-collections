dc78041fe4655e985c7ccf1bf803b8c32abb53da
richsilv@yahoo.co.uk <richardlsilverton32@googlemail.com>
2015-01-05 09:53:39 +0000

    bug fix for undefined options post 1.0.2.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'richsilv:dumb-collections',
-  version: '1.1.2',
+  version: '1.1.3',
   summary: 'Meteor Collections which only sync with the server on request and are saved in local storage.',
   git: 'https://github.com/richsilv/meteor-dumb-collections.git'
 });
